a6ef26d7ae59cf557c25526d16ba6cb552ae62ce
danielars <daniela05rocha@hotmail.com>
2023-02-01 14:55:46 -0500

    mi primer commit

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,12 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   if (x > y || x === y){
+      return x
+   }
+   else {
+      return y
+   }
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +18,13 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if (edad === 18 || edad > 18){
+      return "Allowed"
+   }
+   else {
+      return "Not allowed"
+   }
+   
 }
 
 function conection(status) {
@@ -21,6 +34,7 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   
 }
 
 function saludo(idioma) {
